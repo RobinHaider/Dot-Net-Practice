a4535e83f7bd9f95026de9034de30578b2919ae8
Robin Haider <robinhaider69@gmail.com>
2019-08-21 02:38:57 +0600

    Database Begin

---
+++ UniversityWebApp/UniversityWebApp/IndexUI.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="IndexUI.aspx.cs" Inherits="UniversityWebApp.IndexUI" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="regNoLabel" runat="server" Text="Registation No"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="registationNoTextBox" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="nameLabel" runat="server" Text="Name"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="contactLabel" runat="server" Text="Contact No"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="contactTextBox" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="emailLabel" runat="server" Text="Email"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="emailTextBox" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="departmentLabel" runat="server" Text="Depertment"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="departmentTextBox" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>
+                        <asp:Button ID="registerButton" runat="server" Text="Register" OnClick="registerButton_Click" />
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:Label ID="messageLabel" runat="server" Text="EYES HERE"></asp:Label> 
+        </div>
+        <br />
+        <br />
+        <div>
+            <asp:GridView ID="studentsGridView" runat="server" BackColor="White" BorderColor="#999999" BorderStyle="Solid" BorderWidth="1px" CellPadding="3" ForeColor="Black" GridLines="Vertical">
+                <AlternatingRowStyle BackColor="#CCCCCC" />
+                <FooterStyle BackColor="#CCCCCC" />
+                <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+                <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#808080" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#383838" />
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ UniversityWebApp/UniversityWebApp/IndexUI.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace UniversityWebApp
+{
+    public partial class IndexUI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PopulateStudentList();
+        }
+
+        string connectionString = "Server=ROBINHAIDER; Database=UniversityDB; Integrated Security=True";
+
+        protected void registerButton_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+
+            student.RegistaionNo = registationNoTextBox.Text;
+            student.Name = nameTextBox.Text;
+            student.Contact = contactTextBox.Text;
+            student.Email = emailTextBox.Text;
+            student.Department = departmentTextBox.Text;
+
+            if (DoesRegNoExist(student.RegistaionNo))
+            {
+                messageLabel.Text = "RegNo already exist";
+            }
+            else
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                string query = "INSERT INTO Students VALUES('" + student.RegistaionNo + "','" + student.Name + "','" + student.Contact + "','" + student.Email + "','" + student.Department + "')";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+                int rowAffected = command.ExecuteNonQuery();
+
+                if (rowAffected > 0)
+                {
+                    messageLabel.Text = "Saved Successfully";
+                }
+                else
+                {
+                    messageLabel.Text = "Saving Failed";
+                }
+            }
+
+            PopulateStudentList();
+        }
+
+        private bool DoesRegNoExist(string regNo)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "SELECT * FROM Students WHERE RegistationNo='"+ regNo +"'";
+
+            SqlCommand command = new SqlCommand(query,connection);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            bool hasRows = reader.HasRows;
+
+            reader.Close();
+            connection.Close();
+
+            return hasRows;
+        }
+
+        private void PopulateStudentList()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "SELECT * FROM Students";
+
+            SqlCommand command = new SqlCommand(query,connection);
+
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Student> students = new List<Student>();
+
+            while (reader.Read())
+            {
+                Student student = new Student();
+
+                student.Id = Convert.ToInt32(reader["Id"]);
+                student.RegistaionNo = reader["RegistationNo"].ToString();
+                student.Name = reader["Name"].ToString();
+                student.Contact = reader["ContactNo"].ToString();
+                student.Email = reader["Email"].ToString();
+                student.Department = reader["Department"].ToString();
+
+                students.Add(student);
+            }
+
+            reader.Close();
+            connection.Close();
+
+            studentsGridView.DataSource = students;
+            studentsGridView.DataBind();
+        }
+    }
+}
